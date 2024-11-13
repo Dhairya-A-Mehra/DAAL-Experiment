4d973480a31d80a08555edb273c074048b136196
Dhairya-A-Mehra <mehra.dhairya.btech2022@sitpune.edu.in>
2024-11-13 15:20:57 +0530

    prims code updated

--- Prims-Algorithm.cpp
+++ Prims-Algorithm.cpp
@@ -1,69 +1,89 @@
 #include <iostream>
-#include <climits>
+#define INF 999999
 using namespace std;
+#define V 10  // number of vertices are 10
 
-#define V 5  // Number of vertices in the graph
+// Adjacency matrix creation which will be used for printing
+int S[V][V], n=3;
 
-// A utility function to find the vertex with the minimum key value, from the set of vertices not yet included in the MST
-int minKey(int key[], bool mstSet[]) {
-    int min = INT_MAX, min_index;
+int G[V][V] = {
+    {0, 1, 2},
+    {1, 0, 3},
+    {2, 3, 0}
+};
 
-    for (int v = 0; v < V; v++)
-        if (mstSet[v] == false && key[v] < min)
-            min = key[v], min_index = v;
+int prims() {
+    int cost[V][V], i, j, ne;
+    int min_dist[V], temp_min_dist, visited[V];
+    int from_parent[V], u_present, v_to_be_added;
+    int min_cost_to_return = 0;
 
-    return min_index;
-}
-
-// Function to print the constructed MST stored in parent[]
-void printMST(int parent[], int graph[V][V]) {
-    cout << "Edge \tWeight\n";
-    for (int i = 1; i < V; i++)
-        cout << parent[i] << " - " << i << " \t" << graph[i][parent[i]] << " \n";
-}
-
-// Function to construct and print the MST for a graph represented using adjacency matrix representation
-void primMST(int graph[V][V]) {
-    int parent[V];  // Array to store constructed MST
-    int key[V];     // Key values used to pick minimum weight edge in cut
-    bool mstSet[V]; // To represent set of vertices included in MST
-
-    // Initialize all keys as INFINITE and mstSet[] as false
-    for (int i = 0; i < V; i++)
-        key[i] = INT_MAX, mstSet[i] = false;
-
-    // Always include the first vertex in MST. Make key 0 so that this vertex is picked first
-    key[0] = 0;
-    parent[0] = -1; // First node is always the root of the MST
-
-    // The MST will have V vertices
-    for (int count = 0; count < V - 1; count++) {
-        // Pick the minimum key vertex from the set of vertices not yet included in MST
-        int u = minKey(key, mstSet);
-
-        // Add the picked vertex to the MST set
-        mstSet[u] = true;
-
-        // Update key value and parent index of the adjacent vertices of the picked vertex
-        for (int v = 0; v < V; v++)
-            if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
-                parent[v] = u, key[v] = graph[u][v];
+    // Creation of Cost function and spanning tree
+    for(i =0; i<n; i++) {
+        for (j =0; j<n ; j++){
+            if(G[i][j]==0){
+                cost[i][j]= INF;
+            }
+            else{
+                cost[i][j]= G[i][j];
+            }
+            S[i][j]=0;
+            
+        }
     }
-
-    // Print the constructed MST
-    printMST(parent, graph);
+    
+    // initialize visited[], min_dist[], from_parent[]
+    // source node = 0
+    visited[0]=1;
+    min_dist[0]=0;
+    for(i =1; i<n; i++){
+        min_dist[i]= cost[0][i];
+        from_parent[i]=0;
+        visited[i]=0;
+    }
+    ne = n -1;
+    while(ne>0){
+        
+        // finding min_dist 
+        temp_min_dist = INF;
+        for(i=1; i<n; i++){
+            if(visited[i]==0 && min_dist[i]< temp_min_dist){
+                temp_min_dist= min_dist[i];
+                v_to_be_added = i;
+            }
+        }
+        u_present = from_parent[v_to_be_added];
+        
+        // insert edge in spanning tree & edge decrement & set visited[v]
+        ne --;
+        S[u_present][v_to_be_added] = min_dist[v_to_be_added];
+        S[v_to_be_added][u_present] = min_dist[v_to_be_added];
+        visited[v_to_be_added]=1;
+        
+        // update min_dist[]
+        for(i=1; i<n; i++){
+            if(visited[i]==0 && cost[i][v_to_be_added]< min_dist[i]){
+                min_dist[i]= cost[i][v_to_be_added];
+                from_parent[i]= v_to_be_added;
+            }
+        }
+        min_cost_to_return= min_cost_to_return + cost[u_present][v_to_be_added];
+    }
+    return min_cost_to_return;
 }
 
-int main() {
-    // Example graph represented as an adjacency matrix
-    int graph[V][V] = {{0, 2, 0, 6, 0},
-                       {2, 0, 3, 8, 5},
-                       {0, 3, 0, 0, 7},
-                       {6, 8, 0, 0, 9},
-                       {0, 5, 7, 9, 0}};
-
-    // Print the solution
-    primMST(graph);
-
+int main(){
+    int cost= prims();
+    int i,j;
+    cout << "Spanning tree" << endl;
+    for(i=0; i<n; i++){
+        for(j=0; j<n; j++){
+            cout << S[i][j] << " " << endl;
+        }
+    }
+    
+    cout<< "Minimum Cost of Spanning Tree = "<<cost<<endl;
+    
     return 0;
+    
 }
